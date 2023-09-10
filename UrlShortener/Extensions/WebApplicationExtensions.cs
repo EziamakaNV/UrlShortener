a5aab9e2f91eb84e3bd6401f9c3a63207be1c946
Nnaemeka Eziamaka <Nnaemeka.Eziamaka@stanbicibtc.com>
2023-09-10 20:48:31 +0100

    Add project files.

---
+++ UrlShortener/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UrlShortener.Extensions
+{
+    public static class WebApplicationExtensions
+    {
+        public static void ApplyDatabaseMigrations(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            dbContext.Database.Migrate();
+        }
+    }
+}
